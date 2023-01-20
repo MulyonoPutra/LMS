43649dabf40e951c9d5c8ed1f610436541ba90cd
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-01-20 18:41:31 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import * as swaggerDocument from '../swagger.json';
 dotenv.config();
 
 const app: Express = express();
-const port: string = process.env.PORT ?? '3000';
+const port = process.env.PORT || 3000
 
 cloudinary.config({
 	cloud_name: Environment.cloudinaryName,
